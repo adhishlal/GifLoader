84e5ecf7711265b924fb9437bcf1c195409e4818
Adhish <adhish@adhishlal.com>
2017-02-24 11:12:49 +0530

    classes added

---
+++ gifprogressbar/src/main/java/lal/adhish/gifprogressbar/GifView.java
@@ -0,0 +1,82 @@
+package lal.adhish.gifprogressbar;
+
+/**
+ * Created by Adhish on 24/02/2016.
+ */
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GifView extends View {
+
+    private static final int DEFAULT_MOVIEW_DURATION = 1000;
+
+    private int mMovieResourceId;
+    private Movie mMovie;
+
+    private long mMovieStart = 0;
+    private int mCurrentAnimationTime = 0;
+
+    @SuppressLint("NewApi")
+    public GifView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        /**
+         * Starting from HONEYCOMB have to turn off HardWare acceleration to draw
+         * Movie on Canvas.
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+    }
+
+    public void setImageResource(int mvId){
+        this.mMovieResourceId = mvId;
+        mMovie = Movie.decodeStream(getResources().openRawResource(mMovieResourceId));
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if(mMovie != null){
+            setMeasuredDimension(mMovie.width(), mMovie.height());
+        }else{
+            setMeasuredDimension(getSuggestedMinimumWidth(), getSuggestedMinimumHeight());
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mMovie != null){
+            updateAnimtionTime();
+            drawGif(canvas);
+            invalidate();
+        }else{
+            drawGif(canvas);
+        }
+    }
+
+    private void updateAnimtionTime() {
+        long now = android.os.SystemClock.uptimeMillis();
+
+        if (mMovieStart == 0) {
+            mMovieStart = now;
+        }
+        int dur = mMovie.duration();
+        if (dur == 0) {
+            dur = DEFAULT_MOVIEW_DURATION;
+        }
+        mCurrentAnimationTime = (int) ((now - mMovieStart) % dur);
+    }
+
+    private void drawGif(Canvas canvas) {
+        mMovie.setTime(mCurrentAnimationTime);
+        mMovie.draw(canvas, 0, 0);
+        canvas.restore();
+    }
+
+}
